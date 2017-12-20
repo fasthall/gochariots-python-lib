590f3730660313b2e4998aa8318a89795c364d9a
Wei-Tsung Lin <fasthall@gmail.com>
2017-12-20 23:30:10 +0800

    client for toid version

---
+++ gochariots_toid_client.py
@@ -0,0 +1,49 @@
+import grpc
+import batcherrpc_pb2
+import batcherrpc_pb2_grpc
+from fnvhash import fnv1a_64
+
+class RPCClient:
+    def __init__(self, host):
+        channel = grpc.insecure_channel(host)
+        self.stub = batcherrpc_pb2_grpc.BatcherRPCStub(channel)
+    
+    def postRecord(self, record):
+        rpcRecord = batcherrpc_pb2.RPCRecord(
+            id = record.id,
+            host = record.host,
+            tags = record.tags,
+            causality = batcherrpc_pb2.RPCCausality(
+                host = record.prehost,
+                toid = record.pretoid
+            )
+        )
+        return self.stub.TOIDReceiveRecord(rpcRecord)
+
+    def asyncPostRecord(self, record):
+        rpcRecord = batcherrpc_pb2.RPCRecord(
+            id = record.id,
+            host = record.host,
+            tags = record.tags,
+            causality = batcherrpc_pb2.RPCCausality(
+                host = record.prehost,
+                toid = record.pretoid
+            )
+        )
+        return self.stub.TOIDReceiveRecord.future(rpcRecord)
+
+class Record:
+    """Record consists of key-value pairs and casual dependency"""
+    def __init__(self, id, host):
+        self.id = id
+        self.host = host
+        self.tags = {}
+        self.prehost = 0
+        self.pretoid = 0
+
+    def setParent(self, host, toid):
+        self.prehost = host
+        self.pretoid = toid
+
+    def addTag(self, key, value):
+        self.tags[key] = value
--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import gochariots_client
+import gochariots_toid_client
 import uuid
 
 client = gochariots_client.RPCClient('localhost:9000')
 record = gochariots_client.Record(str(uuid.uuid4()), 1, str(uuid.uuid4()))
 record.addTag('k', 'v')
-
 res = client.asyncPostRecord(record)
+
+toid_client = gochariots_toid_client.RPCClient('localhost:9000')
+record = gochariots_toid_client.Record(str(uuid.uuid4()), 1)
+record.addTag('k', 'v')
+res = toid_client.asyncPostRecord(record)
